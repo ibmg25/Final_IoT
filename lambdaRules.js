07dcd73bbd7cb0a190ad58e27b3dd5a7b008be8a
Ignacio <ignacio.martinez@ucb.edu.bo>
2023-12-11 21:51:51 -0400

    2da version

--- lambdaRules.js
+++ lambdaRules.js
@@ -1,20 +1,22 @@
 const AWS = require('aws-sdk');
 const iotData = new AWS.IotData({ endpoint: 'a3v2gf9whawh53-ats.iot.us-east-2.amazonaws.com' });
+var thing_name;
 
 const TurnOnParams = {
     //topic: `$aws/things/${varGlobalThingName}/shadow/update`,
-    topic: '$aws/things/dispenser/shadow/update',
+    topic: `$aws/things/${thing_name}/shadow/update`,
     //thingName: 'arn:aws:iot:us-east-2:879374154165:thing/dispenser',
     payload: '{"state": {"desired": {"dispenser": "on"}}}',
 };
 
 const TurnOffParams = {
-    topic: '$aws/things/dispenser/shadow/update',
+    topic: `$aws/things/${thing_name}/shadow/update`,
     //thingName: 'arn:aws:iot:us-east-2:879374154165:thing/dispenser',
     payload: '{"state": {"desired": {"dispenser": "off"}}}',
 };
 
 const turnOnDispenser = async () => {
+    console.log(thing_name);
   iotData.publish(TurnOnParams, function(err, data) {
             if (err) console.log(err);
         });
@@ -30,6 +32,7 @@ const turnOffDispenser = async () => {
 
 exports.handler = async (event) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
+    thing_name = event.thing_name;
     const distanceGlassSensor = event.distanceGlassSensor;
     const distanceLiquidSensor = event.distanceLiquidSensor;
     const weight = event.weight;
